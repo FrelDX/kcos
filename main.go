c24f4d77d54f17726d216866530900bcb2eb2b90
caojiaoyue <979477675@qq.com>
2020-07-03 21:03:14 +0800

    *

--- main.go
+++ main.go
@@ -1,26 +1,27 @@
 package main
 
 import (
-	"github.com/gliderlabs/ssh"
 	"github.com/FrelDX/kcos/pty"
+	"github.com/gliderlabs/ssh"
 	"log"
 )
 func main()  {
+
 	sshd()
 
 }
 func sshd()  {
-
 	ssh.Handle(func(s ssh.Session) {
-		pty.MainInterface(s)
+		//pty.MainInterface(s)
+		ss :=pty.NewPtyTerminal(s)
+		ss.Start()
 	})
 	log.Println("starting ssh server on port 2222...")
 	log.Fatal(
 		ssh.ListenAndServe(":2222", nil,
-		ssh.PasswordAuth(func(ctx ssh.Context, pass string) bool {
-		return pass == "12345678"}),
-		ssh.HostKeyFile("./key/id_rsa"),
+			ssh.PasswordAuth(func(ctx ssh.Context, pass string) bool {
+				return pass == "12345678"}),
+			ssh.HostKeyFile("./key/id_rsa"),
 		),
 	)
 }
-
